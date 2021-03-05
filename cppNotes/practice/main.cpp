b730fe05b07bb6fc97ccb18058a20645e56f08eb
naseebpanghal <naseeb_panghal@yahoo.co.in>
2021-03-05 18:49:25 +0530

    .

--- cppNotes/practice/main.cpp
+++ cppNotes/practice/main.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 
+template<typename...> struct Tuple {};
+template<typename T1, typename T2> struct Pair {};
+template<class ...Args1> struct zip {
+    template<class ...Args2> struct with {
+        typedef Tuple<Pair<Args1, Args2>...> type;
+//        Pair<Args1, Args2>... is the pack expansion
+//        Pair<Args1, Args2> is the pattern
+    };
+};
 template <typename T>
 void my_func(T t)
 {
 //  std::cout << "value passed is: " << t << std::endl;
 }
-template <typename T> struct value 
-{
-  T t;
-};
-
-struct another
-{
-  int j;
-  char *k;
-  struct value<int> t;
-};
 int main()
 {
-  struct value<int> temp{10};
-  struct another other{5,"naseeb Panghal",7}; 
-  my_func(temp);
+  std::integral_constant<int, 5> temp;
+  std::integral_constant<int, 10> temp2;
+  temp<int, 5>::type var1;
+//  std::cout << "value: " << temp::value << endl;
+
   return 0;
 }
